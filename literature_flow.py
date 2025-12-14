eb09b3a7bb0f7576842b98dc9bd2b2e88dcb56be
Kun-Lin Ho <abc730814@gmail.com>
2025-12-14 14:21:25 -0500

    chore: Update default `reldays` argument from 365 to 120.

--- literature_flow.py
+++ literature_flow.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Run LiteratureSearch Prefect flow")
     parser.add_argument("--query", dest="query_term", type=str, default=None)
-    parser.add_argument("--reldays", dest="rel_date_days", type=int, default=365)
+    parser.add_argument("--reldays", dest="rel_date_days", type=int, default=120)
     parser.add_argument("--retmax", dest="retmax", type=int, default=30)
     parser.add_argument("--dry-run", dest="dry_run", action="store_true")
     parser.add_argument(
